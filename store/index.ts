fccc4ee8d921aa8751a8380c73e0f8ebdf8c3260
David Lee <davidleeminjae@gmail.com>
2024-08-31 23:51:49 -0400

    Initial Commit

---
+++ store/index.ts
@@ -0,0 +1,44 @@
+import { DriverStore, LocationStore, MarkerData } from "@/types/type";
+import {create} from "zustand";
+
+export const useLocationStore = create<LocationStore>((set) =>({
+    userLatitude: null,
+    userLongitude: null,
+    userAddress: null,
+    destinationAddress: null,
+    destinationLongitude: null,
+    destinationLatitude: null,
+    setUserLocation: ({
+        latitude, longitude, address
+    } : {
+            latitude: number, longitude: number, address: string
+        }) =>{
+        set(()=> ({
+            userAddress : address,
+            userLatitude: latitude,
+            userLongitude: longitude,
+        }))
+    },
+    setDestinationLocation: ({
+        latitude, longitude, address
+    } : {
+            latitude: number, longitude: number, address: string
+        }) =>{
+        set(()=> ({
+            destinationAddress : address,
+            destinationLatitude: latitude,
+            destinationLongitude: longitude,
+        }))
+    },
+}))
+
+export const useDriverStore = create<DriverStore>((set) => ({
+    drivers: [] as MarkerData[],
+    selectedDriver: null,
+    setSelectedDriver: (driverId: number) =>
+      set(() => ({ selectedDriver: driverId })),
+    setDrivers: (drivers: MarkerData[]) => 
+      set(() => ({ drivers: drivers })),
+    clearSelectedDriver: () => 
+      set(() => ({ selectedDriver: null })),
+  }));
